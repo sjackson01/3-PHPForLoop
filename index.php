8a0ea65bc7d16f4ad82e1d0e79ffb40ec8f8c1cc
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-31 13:29:38 -0500

    Add: Practical exercise examples

--- index.php
+++ index.php
@@ -38,13 +38,13 @@ for($currentYear = date('Y'), $year = $currentYear - 100; $year
 }
 */
 
-/*
+
 //Use the date function in place of $currentYear;
 //1919 - 2019
 for($year = date('Y') - 100; $year <= date('Y');$year++){
     echo $year . "<br/>\n";
 }
-*/
+
 
 //Add array code from other chapters  
 $learn = array('Conditionals', 'Arrays', 'Loops',); //Basic Array
@@ -54,11 +54,31 @@ array_unshift($learn, 'HTML', 'CSS');//Add elements to the beginning of array
 asort($learn);//Sort array by value without changing the key
 sort($learn);//Sort array by key before for loop 
 
+/*
 //Set $i = 0 since this array is 0 index
 //Loop while $i is less than the count() of array elements
 //$i iteration expression
 for($i = 0; $i < count();$i++ ){
     echo $learn[$i] . "\n";
+}
+*/
+
+//Practical Exercise 
+//Create a FOR loop that displays each number from 1 to 100.
+//Make sure that you DO NOT show 0 and you DO show 100
+$facts = array(
+    57 => ' on Heinz ketchup bottles represents the number of varieties of pickles the company once had.',
+    2 => ' is the approximate hours a day Giraffes sleeps',
+    18 => ' is the average hours a Python sleeps per day',
+    10 => ' per cent of the world is left-handed.',
+    11 => ' Empire State Buildings, stacked one on top of the other, would be required to measure the Gulf of Mexico at its deepest point.',
+    98 => '% of the atoms in your body are replaced every year',
+    69 => ' is the largest number of recorded children born to one woman',
+);
+//add your loop below this line
+
+for($i=1; $i<=100;$i++){
+echo $i . "\n"; 
 }
 
 ?>
